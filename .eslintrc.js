a2d8f47e797e573d7423ff6a648822c16fe3ec78
Matteo Calabrese <matteo.calabrese1@gmail.com>
2022-07-30 17:33:37 +0300

    Fixes and linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
       'commonjs': true,
       'es2021': true,
       'node': true,
-      "cypress/globals": true
+      'cypress/globals': true
    },
    'extends': 'eslint:recommended',
    'parserOptions': {
